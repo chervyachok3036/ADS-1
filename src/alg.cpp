0e3234c506a410c75268973ae777bd5c2f9bedcd
chervyachok3036 <casha13113@gmail.com>
2026-06-05 21:41:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,28 +2,54 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) {
+    return false;
+  }
+  for (uint64_t i = 2; i * i <= value; ++i) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  uint64_t count = 0;
+  uint64_t num = 1;
+  while (count < n) {
+    ++num;
+    if (checkPrime(num)) {
+      ++count;
+    }
+  }
+  return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t num = value + 1;
+  while (not checkPrime(num)) {
+	  ++num;
+  }
+  return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  for (uint64_t i = 1; i < hbound; ++i) {
+    if (checkPrime(i)) {
+		  sum += i;
+	  }
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t count = 0;
+  for (uint64_t i = lbound; i < hbound; ++i) {
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      ++count;
+    }
+  }
+  return count;
 }
